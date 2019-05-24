6ac3d49b2cbbf86b3e43f26a621d522c37299661
mceylan35 <mehmetceylan2696@gmail.com>
2019-05-24 10:52:37 +0300

    Account Controller login işlemleri

--- AracKiralama.API/Controllers/AccountController.cs
+++ AracKiralama.API/Controllers/AccountController.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Results;
+using System.Web.Routing;
+using AracKiralama.Bll;
+using AracKiralama.Dal.Concrete.EntityFramework.Repository;
+using AracKiralama.Entities;
 
 namespace AracKiralama.API.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/account")]
     public class AccountController : ApiController
     {
+        [HttpPost]
+        [Route("Login")]
+        public IHttpActionResult KullaniciGiris([FromBody]KullaniciGiris kul)
+        {
+            try
+            {
+                using (var kullaniciManager = new KullaniciManager(new EfKullaniciRepository()))
+                {
+                    var kullanici = kullaniciManager.KullaniciGiris(kul.Eposta, kul.Sifre);
+                    return Ok(kullanici);
+                }
+
+            }
+
+
+
+            catch (Exception e)
+            {
+                return Unauthorized();
+            }
+        }
     }
 }
